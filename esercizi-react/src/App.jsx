3e5a03f12c7c7f06882c210bea996b5d86f9ed3d
Elena Lucci <elena.lucci@gmail.com>
2025-06-18 19:50:05 +0200

    Events

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -2,13 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AlertClock from './Components/AlertClock'
 
 function App() {
   
-
+  const handleAlertTime = () => {
+    alert(`Current time is ${new Date().toLocaleTimeString()}`);
+  };
   return (
     <>
-      
+      <AlertClock onAlert={handleAlertTime} />
     </>
   )
 }
---
+++ esercizi-react/src/Components/AlertClock.jsx
@@ -0,0 +1,7 @@
+function AlertClock({onAlert}) {
+    return(
+        <button onClick={onAlert}>Current time</button>
+    )
+}
+
+export default AlertClock
