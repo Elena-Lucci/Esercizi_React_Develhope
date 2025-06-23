9a97a5ac9934239d3c05c76a8150f7f5a546c673
Elena Lucci <elena.lucci@gmail.com>
2025-06-23 12:09:08 +0200

    Modfica chat in chat non statica

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,12 +6,21 @@ import ChatMessage from './Components/ChatMessage'
 
 function App() {
   
-const [messages] = useState([
+const [messages, setMessages] = useState([
     {text: 'Ciao! Come stai? Vieni ad arrampicare questa sera?', sender: 'other' },
     {text: 'Tutto bene, grazie! E tu? Guarda, non saprei, a che ora andate?', sender: 'me' },
     {text: 'Bene anche io, pensavo dopo le 18:00', sender: 'other' },
     {text: 'Perfetto, verso le 15:00 ti do conferma, ma penso di potere!', sender: 'me' },
   ]);
+
+const [otherMessages, setOtherMessages] = useState([
+    {text: 'Ciao! Come stai? Vieni ad arrampicare questa sera?', sender: 'other' },
+    {text: 'Bene anche io, pensavo dopo le 18:00', sender: 'other' },
+  ]);
+
+const [message, setMessage] = useState("")
+
+const [myMessages, setMyMessages] = useState([])
 
   const pageStyle = {
     height: '100vh',
@@ -34,6 +43,20 @@ const [messages] = useState([
     flexDirection: 'column',
   };
 
+  function handleInvia(event) {
+    event.preventDefault()
+    setMessages([...messages, {text: message, sender: "me"}])
+  }
+
+  function handleChange(event) {
+    setMessage(event.target.value)
+  }
+
+  useEffect(()=> {
+    const intervallo = setInterval(()=> setMessages([...messages, {text: "hei", sender: "other"}]), 1000)
+
+   return ()=> clearInterval(intervallo)}, [messages])
+
   return (
     <div style={pageStyle}>
       <div style={chatContainer}>
@@ -41,6 +64,10 @@ const [messages] = useState([
           <ChatMessage message={msg} />
         ))}
       </div>
+      <form onSubmit={handleInvia}>
+        <input type="text" onChange={handleChange}/>
+        <button type='submit'>Invia</button>
+        </form>
     </div>
   );
 }
