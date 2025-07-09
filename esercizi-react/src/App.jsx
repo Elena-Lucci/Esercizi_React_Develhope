3b4e715a0abaec9b39ba0e0205b34b59ad9f9fb1
Elena Lucci <elena.lucci@gmail.com>
2025-07-09 11:54:17 +0200

    PUlizia main

--- esercizi-react/src/App.jsx
+++ esercizi-react/src/App.jsx
@@ -2,17 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { LanguageProvider } from './Context/languageContext';
-import Hello from './Components/Hello';
+
 
 function App() {
 
   return (
-    <div style={{ padding: '2rem' }}>
-      <LanguageProvider>
-        <Hello />
-      </LanguageProvider>
-    </div>
+   <>
+   </>
   );
 };
 
